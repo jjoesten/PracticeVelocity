f0d7127f357d1a1f08418ab5cf2f53013093affe
Joshua Joesten <joshua.joesten@gmail.com>
2017-07-10 23:57:51 -0500

    Revert "Revert "First working version""
    This reverts commit ce6ec336a921c61112d0c5538a2e858341af0a69.

---
+++ PracticeVelocity/PracticeVelocity.Model/IEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeVelocity.Model
+{
+    public interface IEvent
+    {
+        List<string> Items { get; set; }
+    }
+}
